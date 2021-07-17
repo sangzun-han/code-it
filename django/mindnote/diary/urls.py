05bd2c139445722056d99a626ca6fa293ca35037
sangzun-han <hsjun1996@naver.com>
2021-07-17 18:35:37 +0900

    costory 디자인 입히기

--- django/mindnote/diary/urls.py
+++ django/mindnote/diary/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-  path('diary/', views.page_list),
+  path('diary/', views.page_list, name='page-list'),
   # path('diafy/info/', views.info),
   # path('diary/write/', views.page_create),
-  # path('diary/page/<int:page_id>/', views.page_detail),
+  path('diary/page/<int:page_id>/', views.page_detail, name='page-detail'),
   # path('diary/page/<int:page_id>/edit/', views.page_update),
   # path('diary/page/<int:page_id>/delete/', views.page_delete),
 ]
--- django/mindnote/diary/views.py
+++ django/mindnote/diary/views.py
@@ -5,3 +5,7 @@ from .models import Page
 def page_list(request):
     object_list = Page.objects.all() # 데이터 조회
     return render(request, 'diary/page_list.html', {'object_list': object_list})
+
+def page_detail(request, page_id):
+    object = Page.objects.get(id=page_id)
+    return render(request, 'diary/page_detail.html', {'object': object})
