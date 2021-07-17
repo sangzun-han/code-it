aaca8668c79962f1f5c62aac0a233e595c15a7c3
sangzun-han <hsjun1996@naver.com>
2021-07-17 13:59:41 +0900

    django 프로젝트 완성

--- django/costaurant/foods/views.py
+++ django/costaurant/foods/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-
+from django.http import Http404
+from foods.models import Menu
 # Create your views here.
 def index(request):
+  context = dict()
   today = datetime.today().date()
+  menus = Menu.objects.all()
+  
   return render(request, 'foods/index.html',{
-    "today": today
+    "today": today,
+    "menus": menus
     })
 
-def food_detail(request,food):
+def food_detail(request,pk):
   context = dict()
-  if(food == "chicken"):
-    context["name"] = "코딩에 빠진 닭"
-    context["description"] = "주머니가 가벼운 당신의 마음까지 생각한 가격!"
-    context["price"] = 10000
-    context["img_path"] = "foods/images/chicken.jpg"
-  else:
-    return render(request,'foods/detail.html',context=context)
+  menu = Menu.objects.get(id=pk)
+  context["menu"] = menu
+  return render(request,'foods/detail.html',context=context)
   
