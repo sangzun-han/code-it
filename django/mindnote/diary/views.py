13f7d51d8316885aab9af2a93b48a690ad0ff613
sangzun-han <hsjun1996@naver.com>
2021-07-17 17:49:56 +0900

    django 상세페이지 작성

--- django/mindnote/diary/views.py
+++ django/mindnote/diary/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
+def page_list(request):
+    object_list = Page.objects.all() # 데이터 조회
+    return render(request, 'diary/page_list.html', {'object_list': object_list})
