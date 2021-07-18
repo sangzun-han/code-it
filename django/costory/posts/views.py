90a87291af0497f7968ce97ddc86f22ca8b6d733
sangzun-han <hsjun1996@naver.com>
2021-07-18 14:38:06 +0900

    django 상세페이지, 글쓰기 페이지 작성

--- django/costory/posts/views.py
+++ django/costory/posts/views.py
@@ -1,13 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
+from .forms import PostForm
+
 # Create your views here.
 
+
 def post_list(request):
-  posts = Post.objects.all()
-  context = {"posts" : posts}
-  return render(request, 'posts/post_list.html',context)
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, 'posts/post_list.html', context)
+
 
 def post_detail(request, post_id):
-  post = Post.objects.get(id=post_id)
-  context = {"post": post}
-  return render(request, 'posts/post_detail.html',context)
+    post = Post.objects.get(id=post_id)
+    context = {"post": post}
+    return render(request, 'posts/post_detail.html', context)
+
+
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        new_post = Post(
+            title=title,
+            content=content
+        )
+        new_post.save()
+        return redirect('post-detail', post_id=new_post.id)
+    else:
+        post_form = PostForm()
+        return render(request, 'posts/post_form.html', {
+            'form': post_form
+        })
--- django/mindnote/diary/views.py
+++ django/mindnote/diary/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
 from .models import Page
+from .forms import PageForm
 
 # Create your views here.
+
+
 def page_list(request):
-    object_list = Page.objects.all() # 데이터 조회
+    object_list = Page.objects.all()  # 데이터 조회
     return render(request, 'diary/page_list.html', {'object_list': object_list})
+
 
 def page_detail(request, page_id):
     object = Page.objects.get(id=page_id)
     return render(request, 'diary/page_detail.html', {'object': object})
 
+
 def info(request):
     return render(request, 'diary/info.html')
+
+
+def page_create(request):
+    if request.method == 'POST':  # 만약 요청 방식이 POST라면
+        new_page = Page(  # 입력된 데이터를 가져와서 Page 데이터 모델을 만들고
+            title=request.POST['title'],
+            content=request.POST['content'],
+            feeling=request.POST['feeling'],
+            score=request.POST['score'],
+            dt_created=request.POST['dt_created']
+        )
+        new_page.save()  # 데이터베이스에 저장한 후
+    else:  # 만약 요청 방식이 GET이라면
+        form = PageForm()  # 새로운 form을 만들고 (빈 폼)
+        return render(request, 'diary/page_form.html', {'form': form})
+        # 템플릿으로 보내 렌더해서 결과로 돌려줍니다.
