4082f9b0305cdb54ef1ef6426e6907c021ee5076
sangzun-han <hsjun1996@naver.com>
2021-09-01 10:47:59 +0900

    react 가위바위보 간단버전

--- react/dicegame/src/index.js
+++ react/dicegame/src/index.js
@@ -1,3 +1,37 @@
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>안녕 리액트!</h1>, document.getElementById('root'));
+const WINS = {
+  rock: 'scissor',
+  scissor: 'paper',
+  paper: 'rock',
+};
+
+function getResult(left, right) {
+  if (WINS[left] === right) return '승리';
+  else if (left === WINS[right]) return '패배';
+  return '무승부';
+}
+
+function handleClick() {
+  console.log('가위바위보!');
+}
+
+const me = 'rock';
+const other = 'scissor';
+
+ReactDOM.render(
+  <>
+    <h1 id='title'>가위바위보</h1>
+    <h2>{getResult(me, other)}</h2>
+    <button onClick={handleClick} class='hand'>
+      가위
+    </button>
+    <button onClick={handleClick} class='hand'>
+      바위
+    </button>
+    <button onClick={handleClick} class='hand'>
+      보
+    </button>
+  </>,
+  document.getElementById('root')
+);
