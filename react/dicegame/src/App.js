9c2980b4cf5318ad5df40c461e5f3083ebd80162
sangzun-han <hsjun1996@naver.com>
2021-09-06 19:55:17 +0900

    Update : 디자인적용

--- react/dicegame/src/App.js
+++ react/dicegame/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Board from './Board';
 import Button from './Button';
+import './App.css';
 
 function random(n) {
   return Math.ceil(Math.random() * n);
@@ -22,10 +23,14 @@ function App() {
     setOtherHistory([]);
   };
   return (
-    <div>
+    <div className='App'>
       <div>
-        <Button onClick={handleRollClick}>던지기</Button>
-        <Button onClick={handleClearClick}>처음부터</Button>
+        <Button className='App-button' color='blue' onClick={handleRollClick}>
+          던지기
+        </Button>
+        <Button className='App-button' color='red' onClick={handleClearClick}>
+          처음부터
+        </Button>
       </div>
       <div>
         <Board name='나' color='blue' gameHistory={myHistory} />
